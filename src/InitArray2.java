0243ffebc38ecd478ed1d70a87a2664de6687230
danielbanmeke <banmekedaniel@gmail.com>
2023-11-20 17:39:25 +0100

    Update all files

--- src/InitArray2.java
+++ src/InitArray2.java
@@ -1,6 +1,6 @@
 public class InitArray2 {
     public static void main(String[] args) {
-        int[] array = { 32, 27, 64, 18, 95, 14, 90, 70, 60, 37};
+        int[] array = { 32, 27, 64, 18, 95, 14, 90, 70, 60, 37 };
 
         System.out.printf("%s%8s%n", "  Index", "  Value");
 
